78045b1e69de2b9e14ea36143717a24c8ccdaa08
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2022-03-19 02:31:43 +0700

    Bank lookup loading

--- scripts/audio_manager.gd
+++ scripts/audio_manager.gd
@@ -1,6 +1,7 @@
 extends Node
 
 var _packagelist: PoolStringArray = []
+var _banklookup := []
 
 func _ready() -> void:
     GameManager.connect("initialized", self, "init")
@@ -8,6 +9,7 @@ func _ready() -> void:
 func init() -> void:
     print("Initializing Audio Manager")
     _load_packagelist()
+    _load_bank_lookup()
 
 func _load_packagelist() -> void:
     print("Loading SFX packages")
@@ -22,3 +24,30 @@ func _load_packagelist() -> void:
         _packagelist.append(namebuffer.get_string_from_utf8())
     file.close()
     print("Loaded %d SFX package(s)" % _packagelist.size())
+
+func _load_bank_lookup() -> void:
+    print("Loading bank lookup")
+    var file := File.new()
+    var err := file.open(GameManager.game_path + "/audio/CONFIG/BankLkup.dat", File.READ)
+    if err != OK:
+        print("Error opening BankLkup.dat")
+        return
+
+    for i in file.get_len() / 12:
+        var meta := BankMeta.new()
+        meta.package_index = file.get_8()
+        meta.padding.append(file.get_8())
+        meta.padding.append(file.get_8())
+        meta.padding.append(file.get_8())
+        meta.bank_header_offset = file.get_32()
+        meta.bank_size = file.get_32()
+
+        _banklookup.append(meta)
+    file.close()
+    print("Loaded %d bank(s)" % _banklookup.size())
+
+class BankMeta:
+    var package_index: int
+    var padding: PoolIntArray
+    var bank_header_offset: int
+    var bank_size: int
