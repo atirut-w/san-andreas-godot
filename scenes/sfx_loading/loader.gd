378eb1f23b0c96329ee54f96da57dc273ee3f26a
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2022-03-18 21:45:54 +0700

    PakFiles loader prototype

--- scenes/sfx_loading/loader.gd
+++ scenes/sfx_loading/loader.gd
@@ -1,5 +1,7 @@
 extends Control
 
+var packages := []
+
 func _ready() -> void:
     var filediag := FileDialog.new()
     filediag.access = FileDialog.ACCESS_FILESYSTEM
@@ -9,3 +11,20 @@ func _ready() -> void:
     add_child(filediag)
     filediag.popup_centered(Vector2(500, 500))
     var dir = yield(filediag, "dir_selected") # Wait for the user to select a directory
+    
+    _load_pakfiles(dir + "/audio/CONFIG/PakFiles.dat")
+
+func _load_pakfiles(path: String) -> void:
+    print("Loading pakfiles from " + path)
+
+    var file := File.new()
+    var err := file.open(path, File.READ)
+    if err != OK:
+        print("Error opening file: " + String(err))
+        return
+
+    for pi in file.get_len() / 52: # Each package name takes up 52 bytes
+        var namebuffer := file.get_buffer(52) as PoolByteArray
+        packages.append(namebuffer.get_string_from_utf8())
+
+    print("Done")
