b59c6972f5e3eb7d8a1ccc25e94f732ece8344fa
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2022-03-18 22:28:00 +0700

    Directory select dialog

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,2 +1,15 @@
 extends Node
+# You know what this script does.
 
+var game_path := ""
+
+func _ready() -> void:
+    var filediag := FileDialog.new()
+    filediag.access = FileDialog.ACCESS_FILESYSTEM
+    filediag.mode = FileDialog.MODE_OPEN_DIR
+    filediag.window_title = "Select game directory"
+
+    add_child(filediag)
+    filediag.popup_centered_minsize(Vector2(300, 300))
+    game_path = yield(filediag, "dir_selected")
+    print("Game path:" + game_path)
