5f7e99121398caa51b2963524d4fb58788a0bc95
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2022-03-18 23:45:47 +0700

    Package list reading

--- scripts/audio_manager.gd
+++ scripts/audio_manager.gd
@@ -1,4 +1,18 @@
 extends Node
 
+var packagelist := []
+
 func init() -> void:
     print("Initializing Audio Manager")
+    var file := File.new()
+    var err := OK
+
+    err = file.open(GameManager.game_path + "/audio/CONFIG/PakFiles.dat", File.READ)
+    if err != OK:
+        print("Error opening PakFiles.dat")
+        return
+
+    for i in file.get_len() / 52:
+        var namebuffer := file.get_buffer(52) as PoolByteArray
+        packagelist.append(namebuffer.get_string_from_utf8())
+        print("Found SFX package: " + namebuffer.get_string_from_utf8())
