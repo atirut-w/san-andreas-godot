13dc70baedda0c378c6285f491fe1715403dcb18
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2022-03-19 08:27:03 +0700

    Convert indentation to tabs

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -6,14 +6,14 @@ signal initialized()
 var game_path := ""
 
 func _ready() -> void:
-    var filediag := FileDialog.new()
-    filediag.access = FileDialog.ACCESS_FILESYSTEM
-    filediag.mode = FileDialog.MODE_OPEN_DIR
-    filediag.window_title = "Select game directory"
+	var filediag := FileDialog.new()
+	filediag.access = FileDialog.ACCESS_FILESYSTEM
+	filediag.mode = FileDialog.MODE_OPEN_DIR
+	filediag.window_title = "Select game directory"
 
-    add_child(filediag)
-    filediag.popup_centered_minsize(Vector2(800, 300))
-    game_path = yield(filediag, "dir_selected")
-    print("Game path:" + game_path)
+	add_child(filediag)
+	filediag.popup_centered_minsize(Vector2(800, 300))
+	game_path = yield(filediag, "dir_selected")
+	print("Game path:" + game_path)
 
-    emit_signal("initialized")
+	emit_signal("initialized")
